fa2cd885e8cafd03b36c38258d3eb21674234edb
Usmani Daniyal <usmanidaniyal@gmail.com>
2021-03-07 19:15:47 +0200

    set learning steps for inceptionTime and LSTM for ai_croppo dataset

--- dac-noise/networks/config.py
+++ dac-noise/networks/config.py
@@ -23,18 +23,32 @@ def learning_rate(init, epoch):
     
    #return init;
    optim_factor = 0
-   '''
-   print("changing learning rate for inception Time CROP");
+
+   #print("changing learning rate for inception Time CROP");
    # print("keeping it constant thoughout the training process");
-   print("using config steps for inceptiom time")
-   if (epoch > 200):
-       optim_factor = 3;
+  # print("using config steps for inceptiom time")
+   if (epoch > 300):
+       optim_factor = 5;
+   elif (epoch > 280):
+       optim_factor = 5;
+   elif (epoch > 250):
+       optim_factor = 5;
+   elif (epoch > 220):
+       optim_factor = 5;
+   elif (epoch > 200):
+       optim_factor = 5;
+   elif (epoch > 150):
+       optim_factor = 5;
    elif (epoch > 80):
-       optim_factor = 2;
+       optim_factor = 3;
    elif (epoch > 50):
        optim_factor = 1
    elif (epoch > 30):
-       optim_factor = 0;
+       optim_factor = 1;
+   elif (epoch > 20):
+       optim_factor = 1;
+   elif (epoch > 10):
+       optim_factor = 1;
        
 
     
@@ -42,31 +56,38 @@ def learning_rate(init, epoch):
    '''   #print("changing learning rate for lstm");
 # FOR LSTM UCR CROP
 
-   if (epoch > 1900):
-       optim_factor = 0 #4 #7
+   if (epoch > 3000):
+       optim_factor = 7 #4 #7
+   elif (epoch > 1900):
+       optim_factor = 6 #4 #7
    elif (epoch > 1700):
-       optim_factor = 2 #3 #6
+       optim_factor = 5 #3 #6
    elif (epoch > 1500):
-       optim_factor = 0 #2 #5
+       optim_factor = 5 #2 #5
    elif (epoch > 1200):
-       optim_factor = 0 #1 #4
+       optim_factor = 5 #1 #4
    elif (epoch > 950):
-       optim_factor = 1 #3        
+       optim_factor = 5 #3        
    elif (epoch > 750):
-       optim_factor = 0 #2
+       optim_factor = 5 #2
    elif (epoch > 550):
-       optim_factor = 0 #1 #5
+       optim_factor = 5 #1 #5
    elif (epoch > 300):
-       optim_factor = 0 #1#4
+       optim_factor = 5 #1#4
    elif(epoch > 240):
-       optim_factor = 0 #3
+       optim_factor = 4 #3
    elif(epoch > 120):
-       optim_factor = 0 #2
+       optim_factor = 4 #3
+   elif(epoch > 80):
+       optim_factor = 2 #2
+   elif(epoch > 45):
+       optim_factor = 1 #2
     #elif(epoch > 160):
     #    optim_factor = 1'''
   
-   return init*math.pow(0.2, optim_factor)
-   #return init*math.pow(0.5, optim_factor) # for inception Time crop
+   #return init*math.pow(0.2, optim_factor)
+   return init*math.pow(0.5, optim_factor) # for inception Time crop
+   #return init*math.pow(0.2, optim_factor) # for inception Time crop      
 
 def get_hms(seconds):
     m, s = divmod(seconds, 60)
